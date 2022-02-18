f962727d2aa78e959862984042dfaea28da9aabb
Mark Azaev <mark@azaev.space>
2022-02-19 00:24:17 +0300

    Added Java MD5 task from Hackerrank.com

---
+++ src/main/java/com/hackerrank/mark/JavaMD5/Solution.java
@@ -0,0 +1,23 @@
+package com.hackerrank.mark.JavaMD5;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        sc.close();
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(str.getBytes());
+        byte[] digest = md.digest();
+
+        for (byte b : digest) {
+            System.out.format("%02x", b);
+        }
+    }
+}
+
